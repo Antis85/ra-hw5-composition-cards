1a1b08e337b6b786205c838ed26d0b76dedfd859
Anton Bugryakov <antis_amdg@mail.ru>
2021-11-02 23:27:06 +0300

    commit-1-hw-done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/wf6uqvl7rvql32xh/branch/main?svg=true)](https://ci.appveyor.com/project/Antis85/ra-hw5-composition-cards/branch/main)
+
 # ra-hw5-composition-cards
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Cards from './components/Cards';
+import img from './logo512.png';
+
+export default function App() {
+  const imageLink = img;
+  const image = (
+    <img src={imageLink} className="card-img-top" alt="some card pic" />
+  );
+  return (
+    <div className="App">
+      <Cards image={image}>
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <button className="btn btn-primary">Go somewhere</button>
+      </Cards>
+      <Cards>
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <button className="btn btn-primary">Go somewhere else</button>
+      </Cards>
+    </div>
+  );
+}
